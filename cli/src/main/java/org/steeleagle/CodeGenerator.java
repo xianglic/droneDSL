efaa8baad90982d1d674a8854febab2017cb7bed
xianglic <xianglic@andrew.cmu.edu>
2023-09-26 19:44:49 -0400

    Progress

---
+++ cli/src/main/java/org/steeleagle/CodeGenerator.java
@@ -0,0 +1,101 @@
+package org.steeleagle;
+
+import org.aya.intellij.GenericNode;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.steeleagle.parser.BotPsiElementTypes.*;
+
+
+public class CodeGenerator {
+
+//  public static void main(String[] args) {
+//    HashMap<String, String> params = new HashMap<>();
+//    params.put("gimbal_pitch", "45.0");
+//    params.put("coords", "[{\"lat\": 40.4406, \"lng\": -79.9959, \"alt\": 300.0}]");
+//
+//    generateCode(params);
+//  }
+
+  public static void generateCode(GenericNode<? extends GenericNode<?>> node) {
+
+    var gimbal_attr = node.child(TASK).child(TASK_DECL).child(TASK_BODY).child(COMMA_SEP).childrenOfType(ATTRIBUTE).toImmutableSeq().get(1);
+    var gimbal_pitch = gimbal_attr.child(COLONED).child(ATTRIBUTE_EXPR).child(NUMBER).tokenText();
+
+    System.out.println("gimbal_pitch :" + gimbal_pitch);
+
+    List<List<String>> waypoints = new ArrayList<>();
+    var waypoint_attr = node.child(TASK).child(TASK_DECL).child(TASK_BODY).child(COMMA_SEP).childrenOfType(ATTRIBUTE).toImmutableSeq().get(0);
+    var way_points = waypoint_attr.child(COLONED).child(ATTRIBUTE_EXPR).child(SQUARE_BRACKED).child(COMMA_SEP).childrenOfType(PAREN).toSeq(); // childrenOfType(WAYPOINT).toSeq();
+    for (var point : way_points) {
+      List<String> ele = new ArrayList<>();
+      ele.add(point.child(WAYPOINT).child(LATITUDE).tokenText().toString());
+      ele.add(point.child(WAYPOINT).child(LONGITUDE).tokenText().toString());
+      ele.add(point.child(WAYPOINT).child(ALTITUDE).tokenText().toString());
+      waypoints.add(ele);
+    }
+
+//    String gimbalPitch = params.get("gimbal_pitch");
+//    String coords = params.get("coords");
+    StringBuilder code1 = new StringBuilder();
+    code1.append("    public void move() {\n");
+
+    for (var ele : waypoints) {
+      code1.append(String.format("        drone.moveTo(%s, %s, %s);\n", ele.get(0), ele.get(1), ele.get(2)));
+    }
+    code1.append("    }\n");
+
+    String code = String.format("// SPDX-FileCopyrightText: 2023 Carnegie Mellon University - Satyalab\n" +
+        "//\n" +
+        "// SPDX-License-Identifier: GPL-2.0-only\n" +
+        "\n" +
+        "package edu.cmu.cs.dronebrain;\n" +
+        "\n" +
+        "import edu.cmu.cs.dronebrain.interfaces.DroneItf;\n" +
+        "import edu.cmu.cs.dronebrain.interfaces.CloudletItf;\n" +
+        "import edu.cmu.cs.dronebrain.interfaces.Task;\n" +
+        "import java.lang.Float;\n" +
+        "import java.util.HashMap;\n" +
+        "import org.json.JSONArray;\n" +
+        "import org.json.JSONObject;\n" +
+        "import android.util.Log;\n" +
+        "\n" +
+        "public class DetectTask extends Task {\n" +
+        "    \n" +
+        "    Double gimbal;\n" +
+        "    public DetectTask(DroneItf d, CloudletItf c, HashMap<String, String> k) {\n" +
+        "        super(d, c, k);\n" +
+        "        gimbal = %s;\n" +
+        "    }\n" +
+        code1 +
+        "    \n" +
+        "    @Override\n" +
+        "    public void run() {\n" +
+        "        try {\n" +
+        "            drone.setGimbalPose(0.0, gimbal, 0.0);\n" +
+        "            cloudlet.switchModel(\"coco\");\n" +
+        "            Log.d(\"DetectTask\", \"Parsing coordinates...\");\n" +
+        "            move(); \n" +
+        "        } catch (Exception e) {\n" +
+        "            Log.e(\"DetectTask\", e.toString());\n" +
+        "        }\n" +
+        "    }\n" +
+        "\n" +
+        "    @Override\n" +
+        "    public void pause() {}\n" +
+        "\n" +
+        "    @Override\n" +
+        "    public void resume() {}\n" +
+        "}", gimbal_pitch);
+
+    try (var writer = new BufferedWriter(new FileWriter("DetectTask.java"))) {
+      writer.write(code);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
--- cli/src/main/java/org/steeleagle/Main.java
+++ cli/src/main/java/org/steeleagle/Main.java
@@ -5,8 +5,15 @@ import org.jetbrains.annotations.NotNull;
 import org.steeleagle.psi.DslParserImpl;
 import org.steeleagle.psi.StreamReporter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.steeleagle.parser.BotPsiElementTypes.*;
+
 public class Main {
-  public static int bro(GenericNode<? extends GenericNode<?>> node) {
+  public static int translate(GenericNode<? extends GenericNode<?>> node) {
+
+
     return 114514;
   }
 
@@ -18,12 +25,6 @@ public class Main {
                 gimbal_pitch: 20,
                 model: coco
             }
-        
-            Track task2 {
-                waypoints: [(100, 200, 300), (200, 200, 300), (300, 200, 300), (400, 200, 300)],
-                gimbal_pitch: 20,
-                model: robomaster
-            }
         }
         
         Mission {
@@ -31,7 +32,50 @@ public class Main {
         }
         """);
     System.out.println(node.toDebugString());
+//    traverseAST(node);
+    CodeGenerator.generateCode(node);
   }
+//   Define a method to traverse the AST recursively
+  public static void traverseAST(GenericNode<? extends GenericNode<?>> node) {
+    // Check if the current node is null
+    if (node == null) {
+      return;
+    }
+
+    // Process the current node
+    // You can access node properties and perform actions here
+
+//    var gimbal_attr = node.child(TASK).child(TASK_DECL).child(TASK_BODY).child(COMMA_SEP).childrenOfType(ATTRIBUTE).toImmutableSeq().get(1);
+//    var gimbal_pitch = gimbal_attr.child(COLONED).child(ATTRIBUTE_EXPR).child(NUMBER).tokenText();
+////    double gimbal_pitch = gimbal_attr.child(COLONED).child(ATTRIBUTE_EXPR).child(NUMBER).tokenText().toDouble();
+//    System.out.println("gimbal_pitch :" + gimbal_pitch);
+//
+//    List<String> waypoints = new ArrayList<>();
+//    var waypoint_attr = node.child(TASK).child(TASK_DECL).child(TASK_BODY).child(COMMA_SEP).childrenOfType(ATTRIBUTE).toImmutableSeq().get(0);
+//    var way_points = waypoint_attr.child(COLONED).child(ATTRIBUTE_EXPR).child(SQUARE_BRACKED).child(COMMA_SEP).childrenOfType(PAREN).toSeq(); // childrenOfType(WAYPOINT).toSeq();
+//    for (var point : way_points){
+//      waypoints.add(point.child(WAYPOINT).child(LATITUDE).tokenText().toString());
+//      waypoints.add(point.child(WAYPOINT).child(LONGITUDE).tokenText().toString());
+//      waypoints.add(point.child(WAYPOINT).child(ALTITUDE).tokenText().toString());
+//    }
+//    System.out.println("waypoints :" + way_points);
+//    System.out.println("waypoints :" + waypoints);
+//    System.out.println("waypoints :" + waypoints.size());
+
+
+    System.out.println("Mission Type: " + node.child(MISSION).child(MISSION_CONTENT).child(MISSION_START_DECL));
+
+    System.out.println("Node Type: " + node.child(TASK).child(TASK_DECL));
+    System.out.println("Mission Type: " + node.child(MISSION).child(MISSION_CONTENT).child(MISSION_START_DECL));
+
+//    System.out.println("Task Type: " + node.elementType());
+//
+//    // Recursively traverse child nodes
+//    for (var child : node.childrenView()) {
+//      traverseAST(child);
+//    }
+  }
+
 
   @NotNull
   private static DslParserImpl parser() {
