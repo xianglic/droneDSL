b974d1d85c6197d712a4769ea42e01e3370eb5b5
xianglic <xianglic@andrew.cmu.edu>
2023-09-26 19:58:30 -0400

    Delete code

--- cli/src/main/java/org/steeleagle/CodeGenerator.java
+++ cli/src/main/java/org/steeleagle/CodeGenerator.java
@@ -49,48 +49,45 @@ public class CodeGenerator {
     }
     code1.append("    }\n");
 
-    String code = String.format("// SPDX-FileCopyrightText: 2023 Carnegie Mellon University - Satyalab\n" +
-        "//\n" +
-        "// SPDX-License-Identifier: GPL-2.0-only\n" +
-        "\n" +
-        "package edu.cmu.cs.dronebrain;\n" +
-        "\n" +
-        "import edu.cmu.cs.dronebrain.interfaces.DroneItf;\n" +
-        "import edu.cmu.cs.dronebrain.interfaces.CloudletItf;\n" +
-        "import edu.cmu.cs.dronebrain.interfaces.Task;\n" +
-        "import java.lang.Float;\n" +
-        "import java.util.HashMap;\n" +
-        "import org.json.JSONArray;\n" +
-        "import org.json.JSONObject;\n" +
-        "import android.util.Log;\n" +
-        "\n" +
-        "public class DetectTask extends Task {\n" +
-        "    \n" +
-        "    Double gimbal;\n" +
-        "    public DetectTask(DroneItf d, CloudletItf c, HashMap<String, String> k) {\n" +
-        "        super(d, c, k);\n" +
-        "        gimbal = %s;\n" +
-        "    }\n" +
-        code1 +
-        "    \n" +
-        "    @Override\n" +
-        "    public void run() {\n" +
-        "        try {\n" +
-        "            drone.setGimbalPose(0.0, gimbal, 0.0);\n" +
-        "            cloudlet.switchModel(\"coco\");\n" +
-        "            Log.d(\"DetectTask\", \"Parsing coordinates...\");\n" +
-        "            move(); \n" +
-        "        } catch (Exception e) {\n" +
-        "            Log.e(\"DetectTask\", e.toString());\n" +
-        "        }\n" +
-        "    }\n" +
-        "\n" +
-        "    @Override\n" +
-        "    public void pause() {}\n" +
-        "\n" +
-        "    @Override\n" +
-        "    public void resume() {}\n" +
-        "}", gimbal_pitch);
+    String code = String.format("""
+        // SPDX-FileCopyrightText: 2023 Carnegie Mellon University - Satyalab
+        //
+        // SPDX-License-Identifier: GPL-2.0-only
+
+        package edu.cmu.cs.dronebrain;
+
+        import edu.cmu.cs.dronebrain.interfaces.DroneItf;
+        import edu.cmu.cs.dronebrain.interfaces.CloudletItf;
+        import edu.cmu.cs.dronebrain.interfaces.Task;
+        import java.lang.Float;
+        import java.util.HashMap;
+        import org.json.JSONArray;
+        import org.json.JSONObject;
+        import android.util.Log;
+
+        public class DetectTask extends Task {
+          Double gimbal;
+          public DetectTask(DroneItf d, CloudletItf c, HashMap<String, String> k) {
+            super(d, c, k);
+            gimbal = %s;
+          }
+        """ +
+        code1 + """
+          @Override public void run() {
+            try {
+              drone.setGimbalPose(0.0, gimbal, 0.0);
+              cloudlet.switchModel("coco");
+              Log.d("DetectTask", "Parsing coordinates...");
+              move();
+            } catch (Exception e) {
+              Log.e("DetectTask", e.toString());
+            }
+          }
+
+          @Override public void pause() {}
+
+          @Override public void resume() {}
+        }""", gimbal_pitch);
 
     try (var writer = new BufferedWriter(new FileWriter("DetectTask.java"))) {
       writer.write(code);
