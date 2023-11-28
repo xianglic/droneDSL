2df9798d7595f9b0c6d33ff3faf179e5105155cb
xianglic <xianglic@andrew.cmu.edu>
2023-11-27 23:24:11 -0500

    add the post process module

---
+++ postprocess/task-switch/dependencies/Task.py
@@ -0,0 +1,43 @@
+import threading
+import ctypes
+
+class Task(threading.Thread):
+
+    def __init__(self, drone, task_id,**kwargs):
+        threading.Thread.__init__(self)
+        self.drone = drone
+        self.kwargs = kwargs
+        self.task_id = task_id
+
+    # Run is already an abstract method derived from Runnable.
+    # It will be implemented separately by each task.
+    
+    def pause(self):
+        pass
+    
+    def get_task_id(self):
+        return self.task_id
+
+    def _get_id(self):
+        if not self.is_alive():
+            raise threading.ThreadError("the thread is not active")
+
+        # do we have it cached?
+        if hasattr(self, "_thread_id"):
+            return self._thread_id
+
+        # no, look for it in the _active dict
+        for tid, tobj in threading._active.items():
+            if tobj is self:
+                self._thread_id = tid
+                return tid
+
+    def stop(self):
+        thread_id = self._get_id()
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(thread_id), ctypes.py_object(SystemExit))
+        if res > 1:
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+            raise RuntimeError('Error killing task thread')
+
+    def resume(self):
+        pass
---
+++ postprocess/task-switch/runtime/DetectTask.py
@@ -0,0 +1,41 @@
+import threading
+from threadlevel.Task import Task
+import time
+import ast
+
+class DetectTask(Task):
+
+    def __init__(self, drone, task_id, event_queue,**kwargs):
+        super().__init__(drone, task_id, **kwargs)
+        self.event_queue = event_queue
+
+    def trigger_event(self, event):
+        print(f"**************Detect Task: triggered event! {event}**************\n")
+        self.event_queue.put((self.task_id,  event))
+
+    def run(self):
+        # triggered event
+        if (self.task_id == "task1"):
+            # construct the timer with 90 seconds
+            timer = threading.Timer(90, self.trigger_event, ["timeout"])
+            # Start the timer
+            timer.start()
+        try:
+            print(f"**************Detect Task: hi this is detect task {self.task_id}**************\n")
+            coords = ast.literal_eval(self.kwargs["coords"])
+            self.drone.setGimbalPose(0.0, float(self.kwargs["gimbal_pitch"]), 0.0)
+            hover_delay = int(self.kwargs["hover_delay"])
+            for dest in coords:
+                lng = dest["lng"]
+                lat = dest["lat"]
+                alt = dest["alt"]
+                print(f"**************Detect Task: move to {lat}, {lng}, {alt}**************")
+                self.drone.moveTo(lat, lng, alt)
+                time.sleep(hover_delay)
+
+            print("**************Detect Task: Done**************\n")
+            self.trigger_event("done")
+        except Exception as e:
+            print(e)
+
+
