d5d3d38b58b0164eed49777a2a5638933dda0ae5
xianglic <xianglic@andrew.cmu.edu>
2025-05-13 00:31:09 -0400

    fix indentation for track task

--- python/project/task_defs/TrackTask.py
+++ python/project/task_defs/TrackTask.py
@@ -129,8 +129,8 @@ class TrackTask(Task):
         while True:
             result = await self.data.get_compute_result("openscout-object")
             # assume always use the first compute module result
-	    detections = json.loads(result[0])
-	    if len(detections) == 0:
+            detections = json.loads(result[0])
+	    i   if len(detections) == 0:
                 continue
 
             logger.info(f"{detections=}")
