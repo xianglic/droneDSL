2188b07d4fbec9bce46afdd58bdc74afee3f1684
xianglic <xianglic@andrew.cmu.edu>
2025-06-10 22:03:42 -0400

    Upgrade ipc

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,10 @@ subprojects {
   }
   repositories { mavenCentral() }
 
-  val javaVersion = 17
+  val javaVersion = 21
   java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
     toolchain {
       languageVersion.set(JavaLanguageVersion.of(javaVersion))
     }
